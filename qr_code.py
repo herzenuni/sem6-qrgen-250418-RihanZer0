a837f1ca3e2d23d053c8e0afc5bee08aa5e0f06b
RihanZer0 <rinat-sonic@mail.ru>
2018-06-29 13:23:21 +0300

    Create qr_code.py

---
+++ qr_code.py
@@ -0,0 +1,6 @@
+import pyqrcode
+qr_code = pyqrcode.create('https://vk.com/rihan_zer0')
+qr_code.svg('qr_code.svg', scale=7)
+qr_code.eps('qr_code.eps', scale=3)
+
+print(qr_code.terminal(quiet_zone=1))
